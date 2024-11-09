ba14b78c8b1c8ff90fa49d1f8a817c14f47041a4
Kevin Su <972071+ksu006@users.noreply.github.com>
2024-11-08 20:36:11 -0800

    Add wezterm.lua

--- install.zsh
+++ install.zsh
@@ -4,10 +4,7 @@ set -e
 cd ${0:a:h}
 DIR=$(pwd)
 mkdir -pv bak
-for rc in *rc gitconfig tmux.conf; do
+for rc in *rc gitconfig tmux.conf wezterm.lua; do
   [ -e ~/.$rc ] && mv -v ~/.$rc bak/.$rc
   ln -sfv $DIR/$rc ~/.$rc
 done
-[ -e ~/.config/kitty/kitty.conf ] && mv -v ~/.config/kitty/kitty.conf bak/kitty.conf
-ln -sfv $DIR/kitty.conf ~/.config/kitty/kitty.conf
-[ ! -d ~/.config/base16-kitty ] && git clone https://github.com/kdrag0n/base16-kitty.git ~/.config/base16-kitty
